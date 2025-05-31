ad4cc610a8a9fadd295d01736c164e2aa698a8a5
naveeng80 <naveen80.g@gmail.com>
2025-05-31 22:28:40 +0100

    edited 2nd time

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-	res.send('Hello World! Naveen Updated with Helms Charts Automated branch2')
+	res.send('Hello World! Naveen Updated with Helms Charts Automated branch2 code edited')
 })
 
 app.listen(3000, () => console.log('App running on port 3000'))
